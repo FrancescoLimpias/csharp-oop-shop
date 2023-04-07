5fe3f1a116b010cceee67e719a172aa5fdaa3022
Francesco Limpias <francescolimpias@gmail.com>
2023-04-07 14:08:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/* CSHARP OOP SHOP
+ * by Francesco Limpias
+ */
+
+namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
