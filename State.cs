24257c6cc81568ef7b76dc7ad1d9d700fc425317
Francesco Limpias <francescolimpias@gmail.com>
2023-04-09 21:22:39 +0200

    Added State class  - the goal of this class'instances is to store states in a consistent and "trustworthy" way  - added base support for Shops crud

---
+++ State.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* WARNING!
+ * 
+ * this class'methods are not meant to be directly accessed,
+ * instead they should be "underground" accessed by Shop and Product classes.
+ * 
+ */
+
+namespace csharp_oop_shop
+{
+
+    //All stored data (by type, like tables in a database)
+    private List<Shop> shops = new List<Shop>();
+    private List<Product> products = new List<Product>();
+
+    public class State
+    {
+        public State()
+        {
+        }
+
+        //Attempt to retrieve a shop by a given guid
+        public Shop getShopByGuid(Guid guid)
+        {
+            return shops.First(evShop => evShop.guid == shop.guid);
+        }
+
+        //Attempt to store a shop (validation based on guid uniqueness)
+        public void addShop(Shop shop)
+        {
+            if(
+                shops.Contains(shop)  //there is an instance of this shop already
+                || getShopByGuid(shop) != null) //the shop guid has already been registered
+            {
+                throw new Exception("Shop already registered!")
+            }
+
+            shops.add(shop);
+        }
+    }
+
+}
