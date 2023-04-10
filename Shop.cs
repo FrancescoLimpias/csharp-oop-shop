b5fe75fd08147668869d8f42b248276e1d5ca60b
Francesco Limpias <francescolimpias@gmail.com>
2023-04-11 00:55:01 +0200

    Refactored Shop class  -  removed some deprecated methods  - refactored Constructor call (no longer required to "save")  - improved properties mechanics

--- Shop.cs
+++ Shop.cs
@@ -6,73 +6,67 @@ using System.Threading.Tasks;
 
 namespace csharp_oop_shop
 {
-    public class Shop : GuidIdentified
+    public class Shop : IGuidIdentified
     {
 
         //SHOP's PROPERTIES
         //Shop IDs
-        public Guid guid { get; private set; } = new Guid();
-        private List<Guid> productsGuids = new List<Guid>();
+        public Guid Guid { get; private set; } = Guid.NewGuid();
 
         //Shop details
-        public string name, description;
+        public string name, description = "";
+
+        //Shop products
+        public List<Product> Products
+        {
+            get
+            {
+                return Program.currentState.Products.FindAll(product => product.ShopGuid == Guid);
+            }
+        }
 
 
         /* SHOP CONSTRUCTOR(s)
          * 1) base constructor that allows for creation of NEW shops
-         *      1.5) save function that allows for storage of NEW shops
          * 2) function based "constructor" that allows for EXISTENT shops retrieval
          */
         public Shop(string name)
         {
             //save details
             this.name = name;
-        }
-        public Shop save()
-        {
+
             Program.currentState.AddShop(this);
-            return this;
         }
 
-        public static Shop find(Guid guid)
+        public static Shop? Find(Guid guid)
         {
             return Program.currentState.GetShopByGuid(guid);
         }
 
+
         //SHOP's METHODS
         //check if a given product is registered
+        public bool HasProduct(Guid productGuid)
+        {
+            return Products.Find(product => product.Guid == productGuid) != null;
+        }
         public bool HasProduct(Product product)
         {
-            return productsGuids.Contains(product.guid);
+            return HasProduct(product.Guid);
         }
 
-        /* RegisterNewProduct(...args[], int? uniqueCode)
-         * 
-         * this function, given the required Product initial attributes
-         * registers the product (adding the required unique product's code)
-         * in the shop
-         */
-        /*
-        public Product RegisterNewProduct(string name, int price, int iva, int? uniqueCode = null)
+        //STRING CONVERSIONS
+        public override string ToString()
         {
-
-            //uniqueCode validation block
-            if (uniqueCode == null) //No code? Then generate a new one
-            {
-                uniqueCode = GenerateCode();
-            }
-            else if (HasCode((int)uniqueCode)) //Provided code? than check that is acceptable
-            {
-                throw new Exception($"The provided code has already been used! CODE: {uniqueCode}");
-            }
-            int code = (int)uniqueCode;
-
-            //Register the validated code
-            registeredCodes.Add(code);
-
-            return new Product(this, name, price, iva, code);
+            return $"[{Guid}]{name}";
         }
-        */
+        public string ProductsToString()
+        {
+            return string.Join("\n",
+                Products.ConvertAll(new Converter<Product, string>(
+                    product => product.ToString()
+                )).ToArray());
+        }
     }
 
 }
