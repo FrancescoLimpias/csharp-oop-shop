0edb6c7a2850ed8fba2879ffb40238e8424cf8c9
Francesco Limpias <francescolimpias@gmail.com>
2023-04-11 00:45:06 +0200

    Minor fixes

--- GuidIdentified.cs
+++ GuidIdentified.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace csharp_oop_shop
 {
-    public interface GuidIdentified
+    public interface IGuidIdentified
     {
-        public Guid guid { get; }
+        public Guid Guid { get; }
     }
 
     public static class GuidUtils
@@ -18,23 +18,23 @@ namespace csharp_oop_shop
          * this methods are the generic (literally) implementation
          * of validation for all types that have a guid based identification.
          */
-        public static T? GetElementByGuid<T>(List<T> list, Guid guid) where T : GuidIdentified?
+        public static T? GetElementByGuid<T>(List<T> list, Guid guid) where T : IGuidIdentified
         {
             try
             {
-                return list.First(evaluetedElement => evaluetedElement.guid == guid);
+                return list.First(evaluatedElement => evaluatedElement.Guid == guid);
             }
             catch (InvalidOperationException)
             {
-                return default(T); //in other words, NULL
+                return default; //in other words, NULL
             }
         }
 
-        public static void AddElement<T>(List<T> list, T element) where T : GuidIdentified?
+        public static void AddElement<T>(List<T> list, T element) where T : IGuidIdentified
         {
             if (
                 list.Contains(element)  //there is an instance of this element already
-                || GetElementByGuid(list, element.guid) != null) //the element guid has already been registered
+                || GetElementByGuid(list, element.Guid) != null) //the element guid has already been registered
             {
                 throw new Exception("Element already registered!");
             }
