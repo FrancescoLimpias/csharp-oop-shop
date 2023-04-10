4c740adcc9c7e3742def454f6e4afcf3f5d66c2b
Francesco Limpias <francescolimpias@gmail.com>
2023-04-10 12:23:28 +0200

    Adapted Product class for properties paradigm

--- Product.cs
+++ Product.cs
@@ -14,17 +14,26 @@ namespace csharp_oop_shop
      */
     internal class Product
     {
+
+        //PRODUCT's PROPERTIES
         //Product IDs
-        private Guid parentShopGuid;
-        private Guid guid = new Guid();
+        public Guid parentShopGuid { private set; get; }
+        public Guid guid { private set; get; } = new Guid();
 
         //Product details
-        public string name, description;
+        public string 
+            name, 
+            description;
+        public string fullName => $"[{/*GetPaddedUniqueCode()*/"DEBUG PADDING!"}]{name}";
+
+        //Product prices
         public float
             price, /* in euros */
-            iva /* a percentage */;
+            iva; /* a percentage */
+        public float ivaPrice => price + (price * iva / 100); /* in euros */
 
-        /* Product constructor
+
+        /* PRODUCT CONSTRUCTOR
          * - the description attribute is optional
          *   and so is not enforced by the product's constructor.
          */
@@ -32,71 +41,20 @@ namespace csharp_oop_shop
             Shop parentShop,
             string name,
             float price,
-            float iva,
+            float iva
             )
         {
-            this.ParentShop = parentShop.guid;
+            this.parentShopGuid = parentShop.guid;
             this.name = name;
             this.price = price;
             this.iva = iva;
-        }
-
-        /* advanced getters
-         * these are functions that return
-         * "dynamic" attributes, calculated
-         * from the base "static" attributes
-         */
-        public string GetFullName()
-        {
-            return $"[{GetPaddedUniqueCode()}]{name}";
-        }
-        public float GetPrice()
-        {
-            return price;
-        }
-        public float GetIVAPrice()
-        {
-            return price + (price * iva / 100);
-        }
-
-        // GUID getters
-        public Guid GetUniqueCode()
-        {
-            return guid;
-        }
-
-        /* DEPRECATED use
-         * as Guid identifiers have substituted old 'integer based' identifiers
-         * this "normalizing" function lost meaning, now it's kept just for legacy code.
-         */
-        public string GetPaddedUniqueCode()
-        {
-            //Cast the code into a string type
-            string castedCode = guid.ToString();
-
-            //Check if code is not too long
-            /* FROM OLD IMPLEMENTATION, KEPT JUST IN CASE...
-            if (castedCode.Length > ParentShop.GetUniqueCodePadding())
-            {
-                throw new Exception($"The code is too long! CODE: {castedCode}");
-            }
-            */
-
-            //Grow the string until it is long enough
-            //for the requested padding
-            /*
-            while (castedCode.Length < ParentShop.UniqueCodePadding)
-                castedCode = "0" + castedCode;
-            */
-
-            return castedCode;
         }
 
         // Override for Object.toString,
         // so that products have a nice and easy way to be printed on console
         public override string ToString()
         {
-            return $"{GetFullName()}: {GetIVAPrice()}Euro";
+            return $"{fullName}: {ivaPrice}Euro";
         }
     }
 }
